8d5fbdb36f7a8256b3d55a35dadfef94e154ed67
Naitik khoriya <naitikkhoriya144263@gmail.com>
2024-06-20 14:23:59 +0530

    npm i multer API useing to upload the image by the help of frontend to backend

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import multer from "multer";
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extendd: false }));
@@ -10,6 +12,17 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const port = 3000;
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./tmp/my-uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+const upload = multer({ storage: storage });
 app.get("/", async (req, res) => {
   try {
     res.json({ data: "Hello World" });
@@ -17,12 +30,21 @@ app.get("/", async (req, res) => {
     console.log(err);
   }
 });
-app.post("/SignUp", async (req, res) => {
-  try {
-    console.log(req.body);
-    res.json({ data: "Successfully Created" });
-  } catch (err) {
-    console.log(err);
+app.post(
+  "/SignUp",
+  upload.fields([{ name: "avatar", maxCount: 1 }]),
+  async (req, res) => {
+    try {
+      console.log(req.files);
+      const { email, username, Mobile, password } = req.body;
+      console.log(email);
+      console.log(username);
+      console.log(Mobile);
+      console.log(password);
+      res.json({ data: "Successfully Created" });
+    } catch (err) {
+      console.log(err);
+    }
   }
-});
+);
 app.listen(port, () => `Server running on port ${port} 🔥`);
