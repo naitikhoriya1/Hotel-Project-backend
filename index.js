5298b02a68133b5e7f5a90ecaa1c8b83b6807d30
Naitik khoriya <naitikkhoriya144263@gmail.com>
2024-06-21 14:23:09 +0530

    importing bcrypt npm to incrept the password

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import multer from "multer";
+import bcrypt from "bcrypt";
 
 const app = express();
 
@@ -12,9 +13,11 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const port = 3000;
+// Middleware to parse JSON bodies
+app.use(express.json());
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./tmp/my-uploads");
+    cb(null, "./temp/my-uploads");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
@@ -37,13 +40,39 @@ app.post(
     try {
       console.log(req.files);
       const { email, username, Mobile, password } = req.body;
-      console.log(email);
-      console.log(username);
-      console.log(Mobile);
-      console.log(password);
+      const hashedPassword = await bcrypt.hash(password, 10);
+      console.log("email = ", email);
+      console.log("username = ", username);
+      console.log("mobile no = ", Mobile);
+      console.log("password = ", hashedPassword);
       res.json({ data: "Successfully Created" });
+      // res.send("Sign In Successfully");
     } catch (err) {
       console.log(err);
+    }
+  }
+);
+app.post("/SignIn", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    console.log("email = ", email);
+    console.log("password = ", password);
+    res.json({ data: "Successfully Created SignIn" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+app.post(
+  "/BookingForm",
+  upload.fields([{ name: "hotelimage", maxCount: 1 }]),
+  async (req, res) => {
+    try {
+      console.log(req.body);
+      console.log("hotelimage = ", req.files);
+      res.json({ data: "Successfully Created BookingForm" });
+    } catch (err) {
+      res.status(500).json({ error: "Internal Server Error" });
     }
   }
 );
